47e6d7414c1930c7e8de225a13449a15a92c28b8
Paul Boeck <boeck87@gmail.com>
2019-01-10 20:44:40 +0100

    Add speedtest

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 'use strict';
 
 const express = require('express');
+const speedTest = require('speedtest-net');
+
+async function getCurrentSpeed() {
+    return new Promise((resolve, reject) => {
+        const test = speedTest({
+            maxTime: 5000
+        });
+        test.on('data', resolve);
+        test.on('error', reject);
+    });
+}
 
 // Constants
 const PORT = 3000;
@@ -10,6 +21,11 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
+});
+
+app.get('/speedtest', async (req, res) => {
+    const speed = await getCurrentSpeed();
+    res.send(speed);
 });
 
 app.listen(PORT, HOST);
